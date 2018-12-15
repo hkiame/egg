fb013c6f1566f5503dc1f8761c6d417e941ffe1e
Henry Kiame <henrykiame@gmail.com>
2018-12-15 14:34:15 +0300

    parse functions as per page 209

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function parseExpression(program){
+	program = skipSpace(program);
+	let match, expr;
+
+	if(match = /^"([^"]*)"/.exec(program))
+		expr = {type: "value", value: match[1]};
+	else if(match = /^\d+\b/.exec(program))
+		expr = {type: "value", value: match[0]};
+	else if(match = /^[^\s(),"]+/.exec(program))
+		expr = {type: "word", name: match[0]};
+	else
+		throw new SyntaxError(program);
+
+	return parseApply(expr, program.slice(match[0].length));
+}
+
+function skipSpace(string){
+	let first = string.search(/\S/);
+	if(first == -1){ return ""};
+
+	return string.slice(first);
+}
+
+function parseApply(expr, program){
+	program = skipSpace(program);
+
+	if(program[0] != "("){
+		return {expr: expr, rest: program};
+	}
+
+	program = skipSpace(program.slice(1));
+	expr = {type: "apply", operator: expr, args: []};
+
+	while(program[0] != ")"){
+		let arg = parseExpression(program);
+		expr.args.push(arg.expr);
+		program = skipSpace(arg.rest);
+
+		if(program[0] == ","){
+			program = skipSpace(program.slice(1));
+		}else if(program[0] != ")"){
+			throw new SyntaxError("Expected ',' or ')'");
+		}
+	}
+
+	return parseApply(expr, program.slice(1));
+}
