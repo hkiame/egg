8e6efe6b8d12ce6004dc78a2e7089cee1ac68037
Henry Kiame <henrykiame@gmail.com>
2018-12-15 16:56:53 +0300

    included an index.php file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Egg programming language</title>
+</head>
+<body>
+<script type="text/js" src="js/script"></script>
+</body>
+</html>
