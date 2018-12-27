08d87c5fb32f97d08fb221e4b273ea7c7e5bccbb
Henry Kiame <henrykiame@gmail.com>
2018-12-27 12:53:41 +0300

    added application if

--- js/script.js
+++ js/script.js
@@ -55,3 +55,45 @@ function parse(program){
 
 	return result.expr;
 }
+
+function evaluate(expr, env){
+	switch(expr.type){
+		case "value":
+			return expr.value;
+
+		case "word":
+			if(expr.name in env){
+				return env[expr.name];
+			}else{
+				throw new ReferenceError("Undefined variable: " + expr.name);
+			}
+		case "apply":
+		    /*specialForms are like if statement*/
+			if(expr.operator.type == "word" && expr.operator.name in specialForms){
+				return specialForms[expr.operator.name](expr.args, env);
+			}
+
+			let op = evaluate(expr.operator, env);
+			if(typeof op != "function"){
+				throw new TypeError("Applying a non function.");
+			}
+
+			return op.apply(null, expr.args.map((arg)=>{
+				return evaluate(arg, env);
+			}));
+	}
+}
+
+let specialForms = Object.create(null);
+
+specialForms["if"] = function(args, env){
+	if(args.length != 3){
+		throw new SyntaxError("Bad number of args to if");
+	}
+
+	if(evaluate(args[0], env) !== false){
+		return evaluate(args[1], env);
+	}else{
+		return evaluate(args[2], env);
+	}
+};
